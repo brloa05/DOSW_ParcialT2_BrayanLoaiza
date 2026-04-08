91d103ede2acef416a9d7ad487b730753ef7cabb
brayan.loaiza-l <alvarezdairo9@gmail.com>
2026-04-08 12:40:56 -0500

    fix: arreglar conecciones

---
+++ src/main/java/edu/eci/dosw/parcial/DoswParcialT2Application.java
@@ -0,0 +1,12 @@
+package edu.eci.dosw.parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswParcialT2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoswParcialT2Application.class, args);
+    }
+}
